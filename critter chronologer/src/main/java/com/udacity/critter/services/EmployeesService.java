3e388446355bc9952d81cfe11979c4cf3639023b
karthikrocky3010 <rockyneela3010@gmail.com>
2025-04-22 07:49:55 +0530

    Update EmployeesService.java

--- critter chronologer/src/main/java/com/udacity/critter/services/EmployeesService.java
+++ critter chronologer/src/main/java/com/udacity/critter/services/EmployeesService.java
@@ -12,8 +12,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
+@Transactional
 public class EmployeesService {
 
     @Autowired
