5df76f1f89ef3b20e585ebd7432b400c00d31397
karthikrocky3010 <rockyneela3010@gmail.com>
2025-04-22 07:50:48 +0530

    Update SchedulesService.java

--- critter chronologer/src/main/java/com/udacity/critter/services/SchedulesService.java
+++ critter chronologer/src/main/java/com/udacity/critter/services/SchedulesService.java
@@ -10,10 +10,13 @@ import com.udacity.critter.repositories.PetsRepository;
 import com.udacity.critter.repositories.SchedulesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
 @Service
+@Transactional
 public class SchedulesService {
 
     @Autowired
